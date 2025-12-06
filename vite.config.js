c12093fa3fae47b1275a844a702985e311618381
lordAjanaku <patunolaibukundavid001@gmail.com>
2025-12-06 04:01:16 +0000

    intalled tailw & phosphor; created a deploy.yml for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
 				plugins: [['babel-plugin-react-compiler']],
 			},
 		}),
+		tailwindcss(),
 	],
-	base: '/vite-mobile-app/', // Move it here, at the same level as plugins
+	base: '/vite-mobyl-app/', // Move it here, at the same level as plugins
 })
