1461e6ce791b2f00c69e28a7a1202a3474714fcd
lordAjanaku <patunolaibukundavid001@gmail.com>
2025-12-06 14:58:02 +0000

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Navigation from "Navigation.jsx"
+import Navigation from "./Navigation.jsx"
 
 function App() {
 	return (
