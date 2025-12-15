cef9283396541dbb1f70bcc7cea29cc05423c7b3
lordAjanaku <patunolaibukundavid001@gmail.com>
2025-12-15 22:20:50 +0000

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,11 +3,11 @@ import { Eye, EyeSlash, Bell } from "@phosphor-icons/react";
 
 function Home() {
 	return (
-		<div className="px-4">
+		<div className="px-4 mt-8">
 			<header className="flex items-center justify-between">
 				<div className="">
-					<h1 className="text-xl font-bold">Good morning, Terry</h1>
-					<p className="text-base text-gray-500">Welcome to Neobank</p>
+					<h1 className="text-2xl font-bold text-neutral-950">Good morning, Terry</h1>
+					<p className="text-lg text-gray-500">Welcome to Neobank</p>
 				</div>
 				<button className="border border-gray-500 rounded-md p-2" type="button">
 					<Bell size={24} weight="regular" />
@@ -25,13 +25,13 @@ function BankBalance() {
 	return (
 		<div className="p-4">
 			<p className="text-gray-500">Your balance</p>
-			<div className="flex items-center justify-between">
-				<p className="text-2xl text-black">$3,200.00</p>
+			<div className="flex items-center justify-between mb-2">
+				<p className="text-4xl text-neutal-950 font-bold">$3,200.00</p>
 				<button className="" type="button">
 					<EyeSlash size={24} weight="regular" />
 				</button>
 			</div>
-			<button className="bg-black text-white text-semibold width-full p-6" type="button">Add money</button>
+			<button className="bg-neutral-950 text-white font-semibold w-full p-4 rounded-xl" type="button">Add money</button>
 		</div>
 	)
 }
