04dd11ac2b939e880ee16f6ad41263624ea0b098
lordAjanaku <patunolaibukundavid001@gmail.com>
2025-12-15 21:48:18 +0000

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,11 +3,11 @@ import { Eye, EyeSlash, Bell } from "@phosphor-icons/react";
 
 function Home() {
 	return (
-		<>
+		<div className="px-6">
 			<header className="flex items-center justify-between">
 				<div className="">
-					<h1 className="text-xl font-bold">Good morning, Terry</h1>
-					<p className="text-lg text-gray-500">Welcome to Neobank</p>
+					<h1 className="text-base font-bold">Good morning, Terry</h1>
+					<p className="text-base text-gray-500">Welcome to Neobank</p>
 				</div>
 				<button className="border-2 border-gray-500 rounded-md p-2" type="button">
 					<Bell size={24} weight="light" />
