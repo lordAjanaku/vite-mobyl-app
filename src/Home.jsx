ba94e15fd16782438b62de68d1c7a2f670b494ed
lordAjanaku <patunolaibukundavid001@gmail.com>
2025-12-16 15:18:01 +0000

    updates

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,8 +9,8 @@ function Home() {
 					<h1 className="text-2xl font-bold text-neutral-950">Good morning, Terry</h1>
 					<p className="text-lg text-gray-500">Welcome to Neobank</p>
 				</div>
-				<button className="border border-gray-500 rounded-md p-2" type="button">
-					<Bell className="text-red-500" size={24} weight="regular" color="currentColor"/>
+				<button className="border border-gray-300 rounded-md p-3" type="button">
+					<Bell className="text-black" size={24} weight="regular" color="currentColor"/>
 				</button>
 			</header>
 
@@ -22,13 +22,22 @@ function Home() {
 }
 
 function BankBalance() {
+	const [viewBalance, setViewBalance] = useState(false);
+
+	function handleViewBalance() {
+		if (viewBalance) setViewBalance(false);
+		if (!viewBalance) setViewBalance(true);
+	}
+
 	return (
 		<div className="bg-white p-6 my-6 rounded-[2rem]">
 			<p className="text-gray-500">Your balance</p>
 			<div className="flex items-center justify-between mb-4">
 				<p className="text-4xl text-neutral-950 font-bold">$3,200.00</p>
-				<button className="" type="button">
-					<EyeSlash className="text-neutral-950" size={24} weight="regular" color="currentColor"/>
+				<button className="" type="button" onClick={() => handleViewBalance()}>
+					{
+						viewBalance ? <EyeSlash className="text-neutral-950" size={24} weight="regular" color="currentColor"/> : <Eye className="text-neutral-950" size={24} weight="regular" color="currentColor"/>
+					}
 				</button>
 			</div>
 			<button className="bg-black text-white font-semibold w-full p-4 rounded-[15rem]" type="button">Add money</button>
