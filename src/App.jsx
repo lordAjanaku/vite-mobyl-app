82903bdae054a883fa9b3cfb79c97174b335efa1
lordAjanaku <patunolaibukundavid001@gmail.com>
2025-12-16 14:18:17 +0000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./Home.jsx"
 
 function App() {
 	return (
-		<div className="bg-slate-100 px-6 py-10">
+		<div className="bg-slate-100 px-6 py-10 min-h-screen">
 			<Home />
 			<Navigation />
 		</div>
