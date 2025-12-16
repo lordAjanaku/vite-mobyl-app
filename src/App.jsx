17b4f564343599814f81b7a4dc87ad6ef553ce77
lordAjanaku <patunolaibukundavid001@gmail.com>
2025-12-16 14:17:33 +0000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./Home.jsx"
 
 function App() {
 	return (
-		<div className="bg-slate-100">
+		<div className="bg-slate-100 px-6 py-10">
 			<Home />
 			<Navigation />
 		</div>
--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import { Eye, EyeSlash, Bell } from "@phosphor-icons/react";
 
 function Home() {
 	return (
-		<div className="px-4 mt-8">
+		<div className="">
 			<header className="flex items-center justify-between">
 				<div className="">
 					<h1 className="text-2xl font-bold text-neutral-950">Good morning, Terry</h1>
@@ -23,15 +23,15 @@ function Home() {
 
 function BankBalance() {
 	return (
-		<div className="bg-white p-4">
+		<div className="bg-white p-6 my-6">
 			<p className="text-gray-500">Your balance</p>
-			<div className="flex items-center justify-between mb-2">
+			<div className="flex items-center justify-between mb-4">
 				<p className="text-4xl text-neutral-950 font-bold">$3,200.00</p>
 				<button className="" type="button">
 					<EyeSlash size={24} weight="regular" />
 				</button>
 			</div>
-			<button className="bg-black text-white font-semibold w-full p-4 rounded-xl" type="button">Add money</button>
+			<button className="bg-black text-white font-semibold w-full p-4 rounded-[15rem]" type="button">Add money</button>
 		</div>
 	)
 }
